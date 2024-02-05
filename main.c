4f48ae34e283a4832444d343857e0fa7cdd1535c
klein002 <ted.klein@tre.se>
2024-02-05 18:47:04 +0100

    Added support for grow factor

--- main.c
+++ main.c
@@ -23,10 +23,14 @@ typedef double f64;
 typedef size_t (*hash_function)(const void* key, size_t stride);
 typedef int (*compare_function)(const void* a, const void* b, size_t stride);
 
+/// How many percentage of the capacity the hashmap
+/// should be full before it reallocates.
 static const f32 HASHMAP_DEFAULT_LOAD_FACTOR = 0.75f;
+/// How much the hashmap should grow in capacity when
+/// it reallocates.
+static const f32 HASHMAP_DEFAULT_GROW_FACTOR = 2.0f;
 
 
-///
 typedef struct HashMapHeader {
     /// Custom allocator to use for the allocation,
     /// reallocation and deallocation of the hashmap.
@@ -35,14 +39,25 @@ typedef struct HashMapHeader {
     size_t capacity;
     size_t index_mask;
     size_t index_capacity;
-    /// Load factor is a percentage of the capacity.
+
+    /// Load factor is a percentage of the capacity
+    /// before the hashmap will grow.
     /// It is a value between 1 and 100, where 100
     /// means that the hashmap will grow when it is
     /// 100% full.
-    u8     load_factor;
-    u8     key_stride;
-    u8     value_stride;
-    u8     index_stride;
+    u8 load_factor;
+
+    /// Grow factor is a percentage of how much more
+    /// the capacity that the hashmap will grow when
+    /// it is full.
+    /// It is a value between 1 and 250, where 250
+    /// means that the hashmap will grow by 250% of
+    /// its current capacity.
+    u8 grow_factor;
+
+    u8 key_stride;
+    u8 value_stride;
+    u8 index_stride;
 
     // Following this header is:
     // indices[load_factor * capacity * index_stride]
@@ -54,6 +69,12 @@ typedef struct HashMapHeader {
 /// The header is located before the values
 typedef void* HashMap;
 #define HASHMAP_HEADER(hashmap) ((HashMapHeader*)(hashmap) - 1)
+
+static inline
+size_t hashmap_grow_capacity(size_t capacity, u8 grow_factor) {
+    return (size_t)ceil(((f64)grow_factor / 100.0 + 1.0) * (f64) capacity);
+}
+
 
 static inline
 size_t hashmap_index_stride(size_t capacity) {
@@ -85,6 +106,16 @@ int hashmap_set_load_factor(HashMap* map, float load_factor) {
 
     HashMapHeader* header = HASHMAP_HEADER(map);
     header->load_factor = (u8)(load_factor * 100.0f);
+    return 1;
+}
+
+static inline
+int hashmap_set_grow_factor(HashMap* map, float grow_factor) {
+    if (grow_factor < 0.1f || grow_factor > 2.5f)
+        return 0;
+
+    HashMapHeader* header = HASHMAP_HEADER(map);
+    header->grow_factor = (u8)(grow_factor * 100.0f);
     return 1;
 }
 
@@ -128,9 +159,10 @@ HashMap* hashmap_new(struct Allocator* allocator, size_t capacity, float load_fa
     if (load_factor < 0.01f || load_factor > 1.0f || capacity == 0)
         return NULL;
 
-    u8 factor = (u8)(load_factor * 100.0f);
+    u8 load = (u8)(load_factor * 100.0f);
+    u8 grow = (u8)(HASHMAP_DEFAULT_GROW_FACTOR * 100.0f);
 
-    size_t index_capacity = hashmap_index_capacity(capacity, factor);
+    size_t index_capacity = hashmap_index_capacity(capacity, load);
     size_t index_mask     = hashmap_index_mask(index_capacity);
     size_t index_stride   = hashmap_index_stride(index_capacity);
     size_t total_size     = hashmap_total_size(capacity, index_capacity, index_stride, key_stride, value_stride);
@@ -149,7 +181,8 @@ HashMap* hashmap_new(struct Allocator* allocator, size_t capacity, float load_fa
         .capacity       = capacity,
         .index_mask     = index_mask,
         .index_capacity = index_capacity,
-        .load_factor    = factor,
+        .load_factor    = load,
+        .grow_factor    = grow,
         .key_stride     = key_stride,
         .value_stride   = value_stride,
         .index_stride   = index_stride,
@@ -278,8 +311,9 @@ void hashmap_grow(HashMap** map, hash_function hash_key, compare_function compar
     u8     load_factor          = old_header->load_factor;
     size_t key_stride           = old_header->key_stride;
     size_t value_stride         = old_header->value_stride;
+    u8     grow_factor          = old_header->grow_factor;
 
-    size_t new_capacity       = 2 * old_capacity;
+    size_t new_capacity       = hashmap_grow_capacity(old_capacity, grow_factor);
     size_t new_index_capacity = hashmap_index_capacity(new_capacity, load_factor);
     size_t new_index_stride   = hashmap_index_stride(new_index_capacity);
     size_t new_index_mask     = hashmap_index_mask(new_index_capacity);
@@ -302,6 +336,7 @@ void hashmap_grow(HashMap** map, hash_function hash_key, compare_function compar
             .index_capacity = new_index_capacity,
             .index_mask     = new_index_mask,
             .load_factor    = load_factor,
+            .grow_factor    = grow_factor,
             .key_stride     = key_stride,
             .value_stride   = value_stride,
             .index_stride   = new_index_stride,
@@ -366,6 +401,7 @@ int compare_string(const void* key, const void* candidate, size_t stride) {
     static inline int     prefix##_set(Class** map, KEY key, VALUE value)                                             { return hashmap_set((HashMap**)map, (const void*)&key, (const void*)&value, MAP_HASH_FUNCTION, MAP_COMPARE_FUNCTION);  }   \
     static inline void    prefix##_grow(Class** map)                                                                  { hashmap_grow((HashMap**)map, MAP_HASH_FUNCTION, MAP_COMPARE_FUNCTION);  }                                                 \
     static inline int     prefix##_set_load_factor(Class* map, float factor)                                          { return hashmap_set_load_factor((HashMap*)map, factor);  }                                        \
+    static inline int     prefix##_set_grow_factor(Class* map, float factor)                                          { return hashmap_set_grow_factor((HashMap*)map, factor);  }                                        \
     static inline void    prefix##_free(Class** map)                                                                  { hashmap_free((HashMap**)map); }                                                                                           \
 
 
@@ -379,7 +415,11 @@ int main(int argc, const char* argv[]) {
     (void)argc;
     (void)argv;
 
+    // Don't allocate until we reach full capacity.
     StrMap* map = strmap_new_with_load_factor(&allocator_system, 8, 1.0f);
+
+    // Grow by 200% when we reach full capacity.
+    strmap_set_grow_factor(map, 2.0f);
 
     for (int i = 0; i < 0xFFFFF; ++i) {
         int size = (rand() & 31) + 1;
@@ -391,9 +431,15 @@ int main(int argc, const char* argv[]) {
         strmap_set(&map, key, i);
 
         if (i == 1024) {
+            // Grow when we reach 75% of full capacity,
+            // and then grow by 100%.
             strmap_set_load_factor(map, 0.75f);
+            strmap_set_grow_factor(map, 1.0f);
         } else if (i == 2048) {
+            // Grow when we reach 50% of full capacity,
+            // and then grow by 50%.
             strmap_set_load_factor(map, 0.5f);
+            strmap_set_grow_factor(map, 0.5f);
         }
     }
 
